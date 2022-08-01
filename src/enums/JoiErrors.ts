ab53f41fc2511e1b669e727e88392590d2eeb443
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-01 17:15:58 -0300

    chore: add Joi enum for status code

---
+++ src/enums/JoiErrors.ts
@@ -0,0 +1,9 @@
+export enum JoiErrors {
+  'any.required' = 400,
+  'string.base' = 422,
+  'string.min' = 422,
+  'number.base' = 422,
+  'number.min' = 422,
+  'array.base' = 422,
+  'array.includesRequiredUnknowns' = 422,
+}
