067518b48819be84faf868e6d4ebdfefc0e36e83
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-06 13:35:04 -0300

    refactor: refactor function connection to class

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,14 +1,26 @@
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
-import mysql from 'mysql2/promise';
+import mysql, { Pool } from 'mysql2/promise';
 
 const env = dotenv.config();
-dotenvExpand.expand(env)
+dotenvExpand.expand(env);
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-}); 
+class Connection {
+  private static connection: Pool;
 
-export default connection;
+  constructor() {
+    Connection.connection = mysql.createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public async execute(sql: string, value: unknown) {
+    const result = await Connection.connection.execute(sql, value);
+    return result;
+  }
+}
+
+export default new Connection();
