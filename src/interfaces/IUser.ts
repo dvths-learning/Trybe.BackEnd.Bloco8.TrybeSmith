ffea183d01363cb48aa027f5e779f48278a06e7a
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-06 14:12:53 -0300

    refactor: Create an interface for updating User entity data

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -9,4 +9,20 @@ export interface IUserCreateRequest {
   password: string
 
 }
+
+export interface IUserUpdateRequest {
+  username?: string
+  class?: string
+  level?: number
+  password?: string
+}
+/*
+eslint: An interface declaring no members is equivalent to its supertype.
+A razão pela qual essa regra existe é que o typescript usa tipagem estrutural,
+portanto, definir uma interface vazia não ajuda a diferenciar dois tipos.
+Para seguir esse regra eu preciso evitar interfaces vazias
+Meu intuito aqui era tornar flexível o uso de propriedades da interface usando 
+Partial<> para implementar parcialmente a interface no caso de uma atualização de
+dados específicos.
+*/
 export interface IUserUpdateRequest extends Partial<IUserCreateRequest> {}
