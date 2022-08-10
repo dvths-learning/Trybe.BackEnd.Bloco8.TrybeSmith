c98baa4e1956eaccd49d6b8138ba8b509eab5be0
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-09 22:46:05 -0300

    refactor: Add bcrypt as static model method

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -1,6 +1,7 @@
-import { IUser } from '../../interfaces/IUser';
+import * as bcrypt from 'bcrypt';
+import { IUserDTO } from '../../interfaces/IUser';
 
-export default class User implements IUser {
+export default class User implements IUserDTO {
   public readonly id: number;
 
   public username: string;
@@ -18,4 +19,19 @@ export default class User implements IUser {
     this.level = 0;
     this.password = '';
   }
+
+  public static encryptPWD = (password: string) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+  };
+
+  public static checkPWD = (password: string, passwordDB: string) => {
+    const isMatch = bcrypt.compare(password, passwordDB);
+    if (!isMatch) {
+      const e = new Error('Usuário ou senha inválidos');
+      e.name = 'UnauthorizedError';
+      throw e;
+    }
+  };
 }
