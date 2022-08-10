3df40ce6095d1c24a8d1888d1695992e600b003a
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-09 22:58:47 -0300

    chore: Sets the User route in the app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import userRoutes from './routes/UserRoutes'; 
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 const app = express();
 
+const errorMiddleware = new ErrorMiddleware();
+
 app.use(express.json());
+app.use('/users', userRoutes);
+app.use(errorMiddleware.handleError);
 
 export default app;
