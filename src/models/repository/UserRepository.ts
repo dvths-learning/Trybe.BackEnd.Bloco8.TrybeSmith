b5eab9d0fe6a238c2a315e31a15fdb96d25051aa
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-09 22:46:27 -0300

    fix: typo

--- src/models/repository/UserRepository.ts
+++ src/models/repository/UserRepository.ts
@@ -1,11 +1,11 @@
-import { IUser } from '../../interfaces/IUser';
 import Connection from '../Connection';
+import { IUserCreateRequest } from '../../interfaces/IUser';
 
 export default class UserRepository {
-  public static async create(newUser: IUser): Promise<void> {
+  public create = async (newUser: IUserCreateRequest): Promise<void> => {
     await Connection.execute(
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
-      [newUser.username, newUser.classe, newUser.level, newUser.password]
+      [newUser.username, newUser.classe, newUser.level, newUser.password],
     );
-  }
+  };
 }
