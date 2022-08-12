83897c90a346ecddeb9b4d9b355d172dbd741c1f
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-12 17:17:37 -0300

    fix: Remove comments

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -10,13 +10,10 @@ const userValidate = new UserUseCaseValidate();
 
 const userRepository = new UserRepository();
 
-// User service necessita de um repository injetado
 const userService = new UserUseCase(userRepository);
 
-// User controller necessita de um service injetado
 const userController = new UserController(userService);
 
-// TODO chamar o middleware de validação do user
 userRoutes.post('/', userValidate.validate, userController.createUser);
 
 export default userRoutes;
