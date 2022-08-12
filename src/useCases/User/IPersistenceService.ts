78a9799e56d2a950bebdab82354a35e5b5c9d5e7
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-12 17:18:44 -0300

    fix: Changes the void type to a generic type

--- src/useCases/User/IPersistenceService.ts
+++ src/useCases/User/IPersistenceService.ts
@@ -1,5 +1,5 @@
-// Interface para ser substituida por suas implementações
+// Interface para ser substituida por suas implementações: DIP, LSP
 // Um outro serviço que use o método create deverá usar esta mesma interface
 export default interface IPersistenceService<T> {
-  create(entity: T): Promise<void>;
+  create(entity: T): Promise<T>;
 }
