0c92a7402f2db059898e77d083b83abeca7f4d5b
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-22 20:40:39 -0300

    refactor: User entity

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -1,24 +1,25 @@
 import * as bcrypt from 'bcrypt';
-import { IUserDTO } from '../../interfaces/IUser';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import IUser from '../../interfaces/IUser';
 
-export default class User implements IUserDTO {
-  public readonly id: number;
+export default class UserModel {
+  private connection: Pool;
 
-  public username: string;
-
-  public classe: string;
-
-  public level: number;
-
-  public password: string;
-
-  constructor() {
-    this.id = 0;
-    this.username = '';
-    this.classe = '';
-    this.level = 0;
-    this.password = '';
+  constructor(connection: Pool) {
+    this.connection = connection;
   }
+
+  public create = async (newUser: IUser): Promise<IUser> => {
+    const { username, classe, level, password } = newUser;
+    const [result] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
+      [username, classe, level, password],
+    );
+
+    const { insertId } = result;
+
+    return { id: insertId, ...newUser };
+  };
 
   public static encryptPWD = (password: string) => {
     const salt = bcrypt.genSaltSync(10);
