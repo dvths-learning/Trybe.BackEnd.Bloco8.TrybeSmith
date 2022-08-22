1be8d323518154744bdc144f233c538efe69643f
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-22 20:45:42 -0300

    delete: unnecessary file

--- src/useCases/User/IPersistenceService.ts
+++
@@ -1,5 +0,0 @@
-// Interface para ser substituida por suas implementações: DIP, LSP
-// Um outro serviço que use o método create deverá usar esta mesma interface
-export default interface IPersistenceService<T> {
-  create(entity: T): Promise<T>;
-}
