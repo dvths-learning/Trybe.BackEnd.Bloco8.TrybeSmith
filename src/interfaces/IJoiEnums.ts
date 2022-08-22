8806363cfe5d7fd4d0abe26cae034c907f9eb11f
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-22 20:35:19 -0300

    refactor: types joi

--- src/interfaces/IJoiEnums.ts
+++
@@ -1,6 +0,0 @@
-import JoiErrors from '../enums/JoiErrors';
-// https://stackoverflow.com/questions/55377365/what-does-keyof-typeof-mean-in-typescript
-export default interface IJoiError {
-  type: keyof typeof JoiErrors
-  message: string
-}
