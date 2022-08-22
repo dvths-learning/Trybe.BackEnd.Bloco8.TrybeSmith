be9f42138f3f5ef43ea90ea35df9c861db5505d3
Tiago Henrique <tiagohs.dev@gmail.com>
2022-08-22 20:40:10 -0300

    refactor: Order entity

--- src/models/entities/Order.ts
+++ src/models/entities/Order.ts
@@ -1,15 +1,51 @@
-import { IOrder } from '../../interfaces/IOrder';
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import IOrder from '../../interfaces/IOrder';
 
-export default class Order implements IOrder {
-  public readonly id: number;
+export default class OrderModel {
+  private connection: Pool;
 
-  public readonly userId: number;
-
-  public productsIds: number[];
-
-  constructor() {
-    this.id = 0;
-    this.userId = 0;
-    this.productsIds = [];
+  constructor(connection: Pool) {
+    this.connection = connection;
   }
+
+  public getAll = async (): Promise<IOrder[]> => {
+    const [orders] = await this.connection.execute<RowDataPacket[]>(
+      'SELECT * FROM Trybesmith.Orders',
+    );
+
+    const [products] = await this.connection.execute<RowDataPacket[]>(
+      'SELECT * FROM Trybesmith.Products WHERE orderId IS NOT NULL',
+    );
+
+    const productsByOrder: Record<string, number[]> = products
+      .reduce((acc: Record<string, number[]>, product: RowDataPacket) => {
+        const currentOrder: number[] = acc[product.orderId]
+          ? [...acc[product.orderId], product.id] : [product.id];
+        
+        return { ...acc, [product.orderId]: currentOrder };
+      }, {});
+
+    const fullOrders: RowDataPacket[] = orders
+      .map((order: RowDataPacket) => ({ ...order, productsIds: productsByOrder[order.id] }));
+
+    return fullOrders as IOrder[];
+  };
+
+  public create = async (newOrder: IOrder): Promise<IOrder> => {
+    const [result] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+      [newOrder.userId],
+    );
+
+    const { insertId } = result;
+
+    return { id: insertId, ...newOrder };
+  };
+
+  public updateProduct = async (orderId: number, productId: number): Promise<void> => {
+    await this.connection.execute<ResultSetHeader>(
+      'UPDATE Trybesmith.Products SET orderId = ? WHERE id = ?',
+      [orderId, productId],
+    );
+  };
 }
